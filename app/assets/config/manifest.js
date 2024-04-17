72adff797aacbaf928286beccef36d9ef14a1690
Hao Truong <haotv03@gmail.com>
2024-04-17 17:44:28 +0700

    fix js

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,6 +2,3 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
-//= link channels/consumer.js
-//= link channels/index.js
-//= link channels/comment_channel.js
